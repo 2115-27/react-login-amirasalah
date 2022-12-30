154e7c2492faf1c8150ec85624ab9af83d6bfa56
2115-27 <103985748+2115-27@users.noreply.github.com>
2022-12-30 10:53:23 +0530

    Update search.js

--- src/components/Header/Search/search.js
+++ src/components/Header/Search/search.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from '../../../homelike.png';
 class Search extends Component {
   citiesUrl = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cd' +
       'ddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
